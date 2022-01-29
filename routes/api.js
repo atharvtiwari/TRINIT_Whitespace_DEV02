9560b08ec6016d46f132414afb32571dabd26e11
Atharv <atharv.tiwari@outlook.com>
2022-01-29 20:17:05 +0530

    signup implementation

--- routes/api.js
+++ routes/api.js
@@ -19,3 +19,56 @@ router.get('/home', (req, res) => {
         console.log('error: ', error);
     });
 });
+
+router.post("/signup", (req, res) => {
+    var {
+        username,
+        name,
+        email,
+        password
+    } = req.body;
+    
+    if (!username || !email || !password || !name) {
+        return res.status(422).json({
+            error: "please fill all fields",
+        });
+    }
+    bcrypt.hash(password,  Math.floor((Math.random() * 25) + 1) )
+    .then((hashedpass) => {
+        User.findOne({
+            email: email
+        })
+        User.findOne({
+            username: username
+        })
+        .then((savedUser) => {
+            if (savedUser) {
+                return res.status(422).json({
+                    error: "user with this username/email already in database"
+                })
+            }
+            const user = new User({
+                username,
+                name,
+                email,
+                password: hashedpass,
+                current: true
+            })
+            user.save()
+            .then(() => {
+                res.json({
+                    message: "saved successfully"
+                })
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
